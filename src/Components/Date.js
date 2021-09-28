365ca5d7ca9cf22afba9840dc0fe2a41e30d0c34
MIRZA ZIYAD AHMED BEYG <beygz17@gmail.com>
2021-09-28 16:12:26 +0500

    Created Basic 'DATE' Component

---
+++ src/Components/Date.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Date({todayDate}){
+    return(
+        <>
+            <h1>
+                Today is {todayDate}
+            </h1>
+        </>
+    )
+}
+
+export default Date
